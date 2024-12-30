6e06125f3693891c74c8075bf4bd4be06b8b80bc
simonbarrel6 <88184071+simonbarrel6@users.noreply.github.com>
2024-12-30 02:58:18 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const TELEGRAM_TOKEN = '7717821884:AAEVoTHJaEh0IVw7iSNgb_eNFStExuwhNcE';
+export const TELEGRAM_TOKEN = '7495777352:AAEEVSF1K3LA0Qd-u2aPhr_KNZL-YXzpTXQ';
 export const API_ENDPOINT = 'https://tamtam.freewebhostmost.com/apiAhmed.php';
